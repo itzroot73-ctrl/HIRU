873f9fbe580e2510e13b504f1ba77d1877921d32
Itz Root <itzroot73@gmail.com>
2026-01-31 08:39:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4076365317-77702",
+  "appId": "1:846832016084:web:a9ac7aded61f15c79b7941",
+  "apiKey": "AIzaSyAzmfAs0WQ9bxx-eQTpr157zE6WnKMLzjw",
+  "authDomain": "studio-4076365317-77702.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "846832016084"
+};
